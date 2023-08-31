d514fa88f60dd4cee90c4b824a196773225570bd
Tonya Yurina <tonyabatonya@yandex.ru>
2023-08-31 21:26:29 +0300

    all bins are connected in a chain

--- src/main/java/koschei/Application.java
+++ src/main/java/koschei/Application.java
@@ -4,12 +4,16 @@ import koschei.config.AppConfig;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
+import java.util.logging.Logger;
+
 public class Application {
     public static void main(String[] args) {
+        final Logger log = Logger.getLogger(Application.class.getName());
+
         ApplicationContext applicationContext =
                 new AnnotationConfigApplicationContext(AppConfig.class);
         KoscheiTheDeathless koscheiTheDeathless =
                 applicationContext.getBean(KoscheiTheDeathless.class);
-        System.out.println(koscheiTheDeathless.getRulesByDeth());
+        log.info(koscheiTheDeathless.getRulesByDeth());
     }
 }
